19be654a90f99c405924917f52c9b179fb1eae6d
a-dake <a-dake@onu.edu>
2017-01-31 20:07:18 -0500

    Add files via upload

---
+++ FancyHelloWorld/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="UTF-8">
+        <title>Fancy Hello World</title>    
+   
+    </head>
+    <body>
+     <ul>
+         <img src='Earth.png'>
+            <li><a href="default" > These are links</a></li>
+            <li><a href="stuff"> They don't lead anywhere </a></li>
+            <li><a href="bless"> They're just for show</a></li>
+            
+            
+        </ul>
+    
+    <table class="table1" align="right">
+<tbody>
+    <tr><th>
+<strong>Hello World, but Fancy 
+</strong></th></tr>
+</tbody 
+    </table> <br>
+    <table class="table2" align="right"><tbody><tr><th>  
+                    wow look at this quality content!! <br> you could totally add as much text as you wanted here<br>
+                    look more text<br>
+                    it's amazing<br>
+                    <br> <br> i know
+            </th></tr></table>
+         <table class="table1" align="right">
+<tbody>
+    <tr><th>
+<strong>Look, Another Header
+</strong></th></tr>
+</tbody 
+    </table> <br>
+        <table class="table2" align="right"><tbody><tr><th>  
+                and more content!!!!
+            </th></tr></table>
+    
+        
+        
+    </body>
+</html>
