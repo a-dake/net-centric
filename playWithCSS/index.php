1e6a57217a45a46d91c7d4d06d64852af5f96636
a-dake <a-dake@onu.edu>
2017-02-04 23:18:06 -0500

    Add files via upload

---
+++ playWithCSS/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>  
+        <img class="displayed" src="universe.jpg">
+        <ul>
+            <li> <a href="index.php">Home </a> </li>
+            <li> <a href="about.html">About Us </a> </li>
+            <li> <a href="staff.html">Staff</a></li>
+            <li> <a href="news.html">News</a> </li>
+                
+        </ul>
+        
+      
+    <center>  <div><h1>Home</h1> <p> This is a fake webpage for some sort of space themed business.</div> </center>
+       
+    </body>
+</html>
